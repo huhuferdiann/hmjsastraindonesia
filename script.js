5f5987beba7c9f68d20762478f0210b3ffcf989f
huhuferdiann <huhuferdian@gmail.com>
2025-03-29 16:49:57 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Website HMJ Sastra Indonesia siap!");
-    
-    // Menampilkan pesan selamat datang
-    alert("Selamat datang di website HMJ Sastra Indonesia!");
-});
