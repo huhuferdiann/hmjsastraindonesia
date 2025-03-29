c0b49a6eba027a50ca0919921e4b12d16ddf5adc
huhuferdiann <huhuferdian@gmail.com>
2025-03-29 16:24:34 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Website HMJ Sastra Indonesia siap!");
+    
+    // Menampilkan pesan selamat datang
+    alert("Selamat datang di website HMJ Sastra Indonesia!");
+});
